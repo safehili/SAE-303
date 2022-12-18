b28ce1a8fad1045d151bff503d029455329c50ec
Ayoub73 <97605820+Ayoub73@users.noreply.github.com>
2022-12-18 14:39:17 +0100

    premier prototype du site fini

---
+++ SAE303/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.pain').src = anything;
+}
+
+function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
