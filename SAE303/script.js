9f24fd34240b2d750be6748ed88f37092854f141
Ayoub73 <97605820+Ayoub73@users.noreply.github.com>
2022-12-19 14:35:52 +0100

    Retrait du graph, ajout de la typo

---
+++ SAE303/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.pain').src = anything; // Script pour changer l'image
+}
+
+function changeColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color; // Script pour changer la couleur
+}
---
+++ SAE303/statistiques/script.js
@@ -0,0 +1,34 @@
+let data = nb_boulangerie_idf.map((item) => item.nombre) // Donnée, propriété data
+let labels = nb_boulangerie_idf.map((item) => item.dpt) // Libellé, propriété labels
+
+const ctx = document.getElementById('myChart');
+      
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: 'Nombre de boulangers par département en Île-de-France',
+      data: data,
+      backgroundColor: [ 
+        'rgba(255, 26, 104, 0.2)',
+        'rgba(26, 26, 104, 0.2)',
+        'rgba(26, 255, 104, 0.2)',
+        'rgba(104, 26, 255, 0.2)',
+        'rgba(255, 26, 26, 0.2)',
+        'rgba(26, 104, 255, 0.2)',
+        'rgba(200, 255, 104, 0.2)',
+        'rgba(255, 104, 104, 0.2)',
+      ],
+      borderWidth: 1
+      
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
