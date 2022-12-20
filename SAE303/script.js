68b9e6a77dc04989145a505e6e2ac56bbf7129ff
Ayoub73 <97605820+Ayoub73@users.noreply.github.com>
2022-12-20 23:56:16 +0100

    Retrait du script inutile

--- SAE303/script.js
+++
@@ -1,8 +0,0 @@
-function imgSlider(anything){
-    document.querySelector('.pain').src = anything; // Script pour changer l'image
-}
-
-function changeColor(color){
-    const circle = document.querySelector('.circle');
-    circle.style.background = color; // Script pour changer la couleur
-}
